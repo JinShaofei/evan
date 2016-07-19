c34aa0e4228e382bbc9360adda98527fe8a66a50
JinShaofei <jsf1204@163.com>
2016-07-19 16:30:48 +0800

    first commit

---
+++ evan/src/main/java/evan/test/MyThread.java
@@ -0,0 +1,33 @@
+package evan.test;
+
+/**
+ * 
+ * @author shaofeijin
+ *
+ */
+public class MyThread extends Thread {
+
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("------>" + i);
+        }
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+        MyThread m1 = new MyThread();
+        MyThread m2 = new MyThread();
+        m1.start();
+        m2.start();
+
+
+    }
+
+}
---
+++ evan/src/main/java/evan/test/TestController.java
@@ -0,0 +1,30 @@
+package evan.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TestController {
+
+    @RequestMapping("/test.htm")
+    public String test() {
+
+        System.out.println("sdkfslfjsdfsjdlfjs");
+
+        return "test";
+
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public static int t(int n) {
+
+        if (n <= 1) {
+            return 1;
+        } else {
+            return n * t(n - 1);
+        }
+    }
+
+}
