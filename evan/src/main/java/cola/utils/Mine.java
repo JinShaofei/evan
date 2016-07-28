db7804c6379ee5566b147775564e583aded15790
JinShaofei <jsf1204@163.com>
2016-07-28 10:08:23 +0800

    refine code

--- evan/src/main/java/cola/utils/Mine.java
+++ evan/src/main/java/cola/utils/Mine.java
@@ -10,14 +10,14 @@ public class Mine {
     public static void main(String[] args) {
 
         // 插入排序
-        int[] ints = { 31, 13, 11, 7 };
+        int[] ints = { 11, 23, 21, 45, 67, 82, 56 };
         int temp = 0;
         for (int i = 1; i < ints.length; i++) {
             int j = i - 1;
             temp = ints[i]; // 11
             // ints[j] 19
             for (; j >= 0 && temp < ints[j]; j--) {
-                ints[i] = ints[j];
+                ints[j + 1] = ints[j];
             }
             ints[j + 1] = temp;
         }
