fc849da4a50bc5eb3177e95ccda7945ecbcaf9dc
JinShaofei <jsf1204@163.com>
2016-07-28 09:03:36 +0800

    refine code

--- evan/src/main/java/cola/utils/Mine.java
+++ evan/src/main/java/cola/utils/Mine.java
@@ -8,12 +8,21 @@ package cola.utils;
 public class Mine {
 
     public static void main(String[] args) {
-        MyRunnable mr = new MyRunnable();
 
-        Thread a = new Thread(mr, "a");
-        a.start();
-        Thread b = new Thread(mr, "b");
-        b.start();
+        // 插入排序
+        int[] ints = { 31, 13, 11, 7 };
+        int temp = 0;
+        for (int i = 1; i < ints.length; i++) {
+            int j = i - 1;
+            temp = ints[i]; // 11
+            // ints[j] 19
+            for (; j >= 0 && temp < ints[j]; j--) {
+                ints[i] = ints[j];
+            }
+            ints[j + 1] = temp;
+        }
+        for (int i = 0; i < ints.length; i++) {
+            System.out.println(ints[i]);
+        }
     }
-
 }
