065cb036d58e9e0667c4670f9707147063cca8cc
JinShaofei <jsf1204@163.com>
2016-08-02 11:07:34 +0800

    add bootstrap...

---
+++ evan/src/main/java/cola/controller/index/IndexController.java
@@ -0,0 +1,28 @@
+package cola.controller.index;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * index controller
+ * 
+ * @author shaofeijin
+ *
+ */
+@Controller
+public class IndexController {
+
+    /**
+     * to index
+     * 
+     * @return
+     */
+    @RequestMapping("/index.htm")
+    public String index() {
+        return "index";
+    }
+    
+    
+    
+    
+}
